45e40e4dd4f2d0b0e0de6757dfeeb333253b5f06
EmilyPo24-meet <figidave659@gmail.com>
2023-07-17 13:52:55 +0300

    done

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function changeBackgroundColor(color) {
+function changeBackgroundColor() {
+	var color = getRandomColor();
 	document.getElementById('back').style.backgroundColor = color;
+}
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
